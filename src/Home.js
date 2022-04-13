9ef203bd529a08c475f0f39cd64bc1da76d29161
raymundcapule <raymund_capule@yahoo.com>
2022-04-13 15:17:50 +0800

    training code for day 3

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const  [name, setName] = useState ('Emong');
+    const [age, setAge] = useState (39);
+
+    const handleClick = () => {
+        setName('Raymund');
+        setAge(40);
+    }
+   
+       
+
     return (
         <nav className="Home">
             <h2>Home Page</h2>
+            <p>{ name } is { age} years old</p>
+            <button onClick = {  handleClick }>Click</button>
         </nav>
       );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () => {
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
+                
             </div>
         </nav>
       );

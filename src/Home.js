310db5164fb11408138f89be7216d0ecda9b162d
raymundcapule <raymund_capule@yahoo.com>
2022-04-21 12:57:13 +0800

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'New Website', body:'lorem ipsum...', author: 'emong', id: 1},
         { title: 'Welcome Alcoholic!', body:'lorem ipsum...', author: 'raymund', id: 2},
-        { title: 'Web dev ops Alcoholic', body:'lorem ipsum...', author: 'mamon', id: 3}
+        { title: 'Web dev ops', body:'lorem ipsum...', author: 'emong', id: 3},
+        { title: 'Web dev ops Alcoholic', body:'lorem ipsum...', author: 'emong', id: 4}
     ]);
 
     return (
         <div className="home">
           <BlogList blogs={blogs} title = "Lahat ng Blogs!" />
+          <BlogList blogs={blogs.filter((blogs) => blogs.author === 'raymund')} title = "raymund's Blogs!" />
         </div>
     ) 
 }

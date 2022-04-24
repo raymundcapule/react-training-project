30f0ebc6f13dd39d1ba5f35cc8e5f70aaec9027a
raymundcapule <raymund_capule@yahoo.com>
2022-04-24 13:44:00 +0800

    useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,11 @@ const Home = () => {
         const newBlogs = blogs.filter (blogs => blogs.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() =>{
+        console.log ('use effect');
+        console.log(blogs);
+    });
 
     return (
         <div className="home">

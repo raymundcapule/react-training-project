083df8ba2b2f5c7b5e3df82d99c28c9353f7b9c4
raymundcapule <raymund_capule@yahoo.com>
2022-04-25 09:28:36 +0800

    Fetching Data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,16 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'New Website', body:'lorem ipsum...', author: 'emong', id: 1},
-        { title: 'Welcome Alcoholic!', body:'lorem ipsum...', author: 'raymund', id: 2},
-        { title: 'Web dev ops', body:'lorem ipsum...', author: 'emong', id: 3}
-    ]);
-
-    const [name, setName] = useState('emong');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter (blogs => blogs.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() =>{
-        console.log ('use effect');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            }) 
+            .then(data => {
+                setBlogs(data);
+            })
     }, []);
 
     return (

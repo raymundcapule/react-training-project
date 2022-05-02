674621644454092c356bcb387b05a25f7850bfa2
jun-gh <miabanes@gmail.com>
2022-05-03 02:10:27 +0800

    Change from BrowseRouter to HashRouter for history

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div> 
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
